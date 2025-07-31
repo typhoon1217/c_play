61a496a179cb8c199de41dfd0b78a9a1467a5e67
JungwooShin <jungwooshin.97@gmail.com>
2025-07-31 22:10:30 +0900

    const

--- ch2/base/const/main.c
+++ ch2/base/const/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(void)
-{
-    printf("%c\n", 'A');
-    printf("%s\n", "A");
-    printf("%c은 %")
+int main(void) {
+  printf("%c\n", 'A');
+  printf("%s\n", "A");
+  printf("%c은 %s입니다.\n", '1', "first");
+  return 0;
 }
