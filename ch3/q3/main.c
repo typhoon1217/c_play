229a3ceaa32a2db0b5e684373ec9f58f3b3e89a8
JungwooShin <jungwooshin.97@gmail.com>
2025-08-01 20:23:41 +0900

    3.q3

---
+++ ch3/q3/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int kor, eng, math;
+  int total;
+
+  kor = 70;
+  eng = 80;
+  math = 90;
+
+  total = kor + eng + math;
+
+  printf("total : %d\n", total);
+
+  return 0;
+}
