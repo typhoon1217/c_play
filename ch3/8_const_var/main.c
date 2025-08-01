c9ab0e256f04011c3e8b9fd219fd48c06ef67205
JungwooShin <jungwooshin.97@gmail.com>
2025-08-01 20:23:19 +0900

    3.8

---
+++ ch3/8_const_var/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int income = 0;
+  double tax;
+  const double tax_rate = 0.12;
+
+  // tax_rate = 0.12;
+
+  income = 456;
+
+  tax = income * tax_rate;
+  printf("tax: %.1lf\n", tax);
+
+  return 0;
+}
